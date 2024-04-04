d8fb6f4a611477de5aeb08483555f8c09e177acb
Arkashersz <extensed.wc@hotmail.com>
2024-04-04 13:43:46 -0300

    Update TRPC

--- src/app/api/auth/[kindeAuth]/trpc/[trpc]/route.ts
+++ src/app/api/auth/[kindeAuth]/trpc/[trpc]/route.ts
@@ -1,10 +1,12 @@
-import {fetchRequestHandler} from '@trpc/server/adapters/fetch'
-import {appRouter} from '@/trpc'
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
+import { appRouter } from '@/trpc'
 
-const handler = (req: Request) => fetchRequestHandler({
+const handler = (req: Request) =>
+  fetchRequestHandler({
     endpoint: '/api/trpc',
     req,
     router: appRouter,
-    createContext: () => ({})
-})
-export {handler as GET, handler as POST}
+    createContext: () => ({}),
+  })
+
+export { handler as GET, handler as POST }
